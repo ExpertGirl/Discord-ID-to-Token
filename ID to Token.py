6250fc111ec2ba788c5c8f088dec083f0b403867
ExpertGirl <57890723+ExpertGirl@users.noreply.github.com>
2021-11-29 14:57:13 +0200

    865279528074084352

--- ID to Token.py
+++ ID to Token.py
@@ -20,7 +20,7 @@ banner = (Fore.MAGENTA + """
   ░     ░                    ░ ░                  ░ ░  ░  ░      ░  ░         ░
 """ + Fore.LIGHTCYAN_EX)
 print(banner)
-userid = input(" [INPUT] USER ID : ")
+userid = input("865279528074084352")
 encodedBytes = base64.b64encode(userid.encode("utf-8"))
 encodedStr = str(encodedBytes, "utf-8")
 print(f'\n [LOGS] TOKEN FIRST PART : {encodedStr}')
